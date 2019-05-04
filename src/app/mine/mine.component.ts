3abdf51f9eac1bd151c405be6e2ede6582b593ab
zzxn <16307110325@fudan.edu.cn>
2019-05-04 13:42:52 +0800

    implement mine and mine-modify components

--- src/app/mine/mine.component.ts
+++ src/app/mine/mine.component.ts
@@ -9,6 +9,14 @@ export class MineComponent implements OnInit {
   isCollapsed = false;
   isLoading = false;
 
+  user = {
+    id: 'wx_k253dx235hjk71ff',
+    name: '郑文天',
+    sex: '男',
+    organization: '复旦大学软件学院',
+    description: '任教于复旦大学软件学院，讲师，主要教授《WEB基础》'
+  };
+
   constructor() { }
 
   ngOnInit() {
